5d49c61e94104438f339eabd9870b69ade6966b9
shiloh <shilohlee98@gmail.com>
2024-12-14 04:26:15 +0800

    Revert "Database Initialize"

--- src/app/controllers/home_controller.py
+++
@@ -1,9 +0,0 @@
-from flask import jsonify
-import logging
-
-class HomeController:
-    def index(self):
-        logging.info("----HomeController.index----")
-        return jsonify({"message": "Welcome!"})
-    
-home_controller = HomeController()
