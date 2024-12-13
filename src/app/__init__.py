056c0a77d6f85dbec766ef0eb16953ac17c314ce
JLin <jlin890516@gmail.com>
2024-12-14 01:21:31 +0800

    feat:add every feature controllers and routes

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -1,5 +1,10 @@
 import os
 from flask import Flask
+from app.routes.auth_routes import auth_bp
+from app.routes.book_routes import book_bp
+from app.routes.post_routes import post_bp
+from app.routes.reply_routes import reply_bp
+
 
 def create_app():
     # create and configure the app
@@ -13,5 +18,10 @@ def create_app():
     @app.route('/hello')
     def hello():
         return 'Hello, World!'
-
+    
+    app.register_blueprint(auth_bp, url_prefix='/auth')
+    app.register_blueprint(book_bp, url_prefix='/book')
+    app.register_blueprint(post_bp, url_prefix='/post')
+    app.register_blueprint(reply_bp, url_prefix='/reply')
+    
     return app
